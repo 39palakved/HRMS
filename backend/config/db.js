87126ce67361f15426e8a0b56a38ee7644b8b7c1
palak porwal <porwalpalak10@gmail.com>
2025-08-21 10:28:35 +0530

    new commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,11 @@ dotenv.config()
 main().catch(err => console.error("MongoDB connection error:", err))
 
 async function main() {
-  await mongoose.connect(process.env.MONGODB_URI)
+  await mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 30000, 
+})
   console.log("✅ DB connected successfully")
 }
 
